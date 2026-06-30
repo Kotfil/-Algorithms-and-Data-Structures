a5d4178cbd327e654aab28da5df0617acd0ad2e6
filipp <filippkotenko@outlook.com>
2026-06-30 19:44:39 +0300

    feat: add Two Sum II solution to algorithm practice collection

---
+++ neetcode/two-sum-ii.js
@@ -0,0 +1,36 @@
+class Solution {
+    /**
+     * @param {number[]} numbers
+     * @param {number} target
+     * @return {number[]}
+     */
+    twoSum(numbers, target) {
+        let left = 0;
+        let right = numbers.length - 1;
+
+        while (left < right) {
+            const sum = numbers[left] + numbers[right];
+
+            if (sum === target) {
+                // Задача требует вернуть индексы в формате 1-indexed (т.е. счет с 1, а не с 0)
+                return [left + 1, right + 1];
+            } 
+            
+            if (sum > target) {
+                // Если сумма получилась слишком большой, нам нужно её уменьшить.
+                // Из-за того что массив отсортирован по возрастанию, мы сдвигаем правый указатель влево.
+                right--;
+            } else {
+                // Если сумма слишком маленькая, нам нужно её увеличить.
+                // Сдвигаем левый указатель вправо.
+                left++;
+            }
+        }
+        
+        return [];
+    }
+}
+
+// Пример использования:
+// const solution = new Solution();
+// console.log(solution.twoSum([1, 2, 3, 4], 3)); // Выведет: [1, 2]
